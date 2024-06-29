f812a7b92b99b7e3702766bd5218bdf126270127
criistiiaan <dadarevalo6@gmail.com>
2024-06-28 23:44:10 -0500

    create the conversation view on list tittle u can see how to reference it :)

--- lib/main.dart
+++ lib/main.dart
@@ -242,8 +242,11 @@ appBar: AppBar(
             trailing: Text('12:52 pm'),
             selected: true,
             onTap: () {
-              // Acción al hacer clic en Patricia Estrella
-            },
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => DetailsPage()),
+                );
+              },
           ),
           ListTile(
             leading: CircleAvatar( backgroundColor: Color.fromARGB(255, 34, 72, 33),
@@ -282,6 +285,115 @@ appBar: AppBar(
         onTap: (index) {
           // Acción al seleccionar un elemento del BottomNavigationBar
         },
+      ),
+    );
+  }
+}
+
+
+
+class DetailsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100.0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            // Acción para volver atrás
+            Navigator.pop(context);
+          },
+        ),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start, // Align to the start (left)
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 100.0), // Adjust the left padding as needed
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.account_circle, size: 50),
+                  SizedBox(height: 7),
+                  Text('Luis Castro', style: TextStyle(fontSize: 16.0)),
+                ],
+              ),
+            ),
+          ],
+        ),
+        centerTitle: false, // Set to false since we're aligning manually
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              reverse: true,  // This will keep the messages at the bottom
+              children: <Widget>[
+                _buildMessageRow('Sí, está disponible', true),
+                _buildMessageRow('Hola', true),
+                _buildMessageRow('Hola, ¿Está disponible?', false),
+              ],
+            ),
+          ),
+          _buildMessageInput(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMessageRow(String message, bool isSentByMe) {
+    final alignment = isSentByMe ? CrossAxisAlignment.end : CrossAxisAlignment.start;
+    final color = isSentByMe ? Color.fromARGB(255, 34, 72, 33) : Color.fromARGB(255, 235, 253, 228);
+    final textColor = isSentByMe ? Colors.white : Colors.black; // Change text color based on sender
+    final textSize = 15.0; // Adjust text size as needed
+
+    return Align(
+      alignment: isSentByMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text(
+          message,
+          style: TextStyle(
+            color: textColor,
+            fontSize: textSize,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMessageInput() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          top: BorderSide(color: Colors.grey[200]!),
+        ),
+      ),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Escribe aquí...',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            onPressed: () {
+              // Acción para enviar el mensaje
+            },
+          ),
+        ],
       ),
     );
   }
