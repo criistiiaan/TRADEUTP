06513b42971cc15d770a136ed491ddb8e4b6eacd
criistiiaan <dadarevalo6@gmail.com>
2024-06-28 22:26:26 -0500

    add the messages view, if u can help with the images would be great :) i get stuck on it

--- lib/main.dart
+++ lib/main.dart
@@ -110,6 +110,7 @@ class BottomNavTabs {
   ];
 }
 
+//clase para la ventanan home
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -119,6 +120,7 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
+// clase para la ventana de favoritos
 class FavoritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -185,6 +187,7 @@ class FavoritesScreen extends StatelessWidget {
   }
 }
 
+// Clase para la ventana de notificaciones
 class NotificationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -194,11 +197,92 @@ class NotificationsScreen extends StatelessWidget {
   }
 }
 
+// Clase para la ventana de mensajes
 class MessagesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Messages Screen'),
+    return Scaffold(
+appBar: AppBar(
+        title: Text(
+          'Mensajes',
+          style: TextStyle(
+            color: Color.fromARGB(255, 34, 72, 33),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.white, // Cambia esto si necesitas otro color de fondo
+        iconTheme: IconThemeData(color: Color.fromARGB(255, 34, 72, 33)), // Cambia el color de los íconos del AppBar
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Color.fromARGB(255, 34, 72, 33), // Color del borde
+                  width: 2.0, // Ancho del borde
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(4.0), // Espaciado entre el ícono y el borde
+                child: Icon(Icons.person, color: Color.fromARGB(255, 34, 72, 33)),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: CircleAvatar( backgroundColor: Color.fromARGB(255, 34, 72, 33),
+              child: Icon(Icons.person, color: Color.fromARGB(255, 255, 255, 255)),
+            ),
+            title: Text('Luis Castro'),
+            subtitle: Text('Sí está disponible.'),
+            trailing: Text('12:52 pm'),
+            selected: true,
+            onTap: () {
+              // Acción al hacer clic en Patricia Estrella
+            },
+          ),
+          ListTile(
+            leading: CircleAvatar( backgroundColor: Color.fromARGB(255, 34, 72, 33),
+              child: Icon(Icons.person, color: Color.fromARGB(255, 255, 255, 255)),
+            ),
+            title: Text('Patricia Estrella'),
+            subtitle: Text('Hola, ¿Está disponible?'),
+            trailing: Text('12:52 pm'),
+            selected: true,
+            onTap: () {
+              // Acción al hacer clic en Patricia Estrella
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favoritos',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Notificaciones',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Mensajes',
+          ),
+        ],
+        currentIndex: 3, // Índice del elemento seleccionado
+        onTap: (index) {
+          // Acción al seleccionar un elemento del BottomNavigationBar
+        },
+      ),
     );
   }
 }
